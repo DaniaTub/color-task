f9e1d6ec6580d6f62156034f9e253b15db5d80a4
DaniaTub <46869071+DaniaTub@users.noreply.github.com>
2021-02-13 13:37:50 +0200

    Create ColorCellModule.js

---
+++ ColorCellModule.js
@@ -0,0 +1,11 @@
+export default class ColorCellModel {
+    color;
+    description;
+    name;
+
+    constructor(color, description, name) {
+        this.color = color;
+        this.description = description;
+        this.name = name;
+    }
+}
